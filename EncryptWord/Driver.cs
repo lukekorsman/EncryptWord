42233b1c9ae65e0478e0359735d45f2eb1fa9764
lukekorsman <korsmanl@seattleu.edu>
2018-04-29 11:41:17 -0700

    Updated Driver.cs with test functions. Updated Driver.cs and Main.cs with comments

--- EncryptWord/Driver.cs
+++ EncryptWord/Driver.cs
@@ -3,6 +3,9 @@
 // DATE: 26Apr2018
 // VERSION: 1
 
+// This file contains functions which test the EncryptWord.cs file. Each 
+// will test a different piece of code of the EncryptWord class.
+
 using System;
 using encryption;
 
@@ -10,32 +13,42 @@ namespace DriverFile
 {
     public class Driver
     {
+        // Test to ensure EncryptWord initialize properly
         public static string testObjectInitialState()
         {
             encryption.encryptWord initialState = new encryptWord();
+
+            // Initial state will have all guess statistics as 0's
             return initialState.DisplayGuessStatistics();
         }
 
+        // Test to ensure encryption works for x, y and z. 
         public static string testEncryptEndOfAlphabet()
         {
             encryption.encryptWord alphabetTest = new encryptWord();
             string testWord = "xxyyzz";
 
+            // Returned string will be "aabbcc"
             return alphabetTest.Encrypt(testWord);
         }
 
+        // Test to ensure strings less than 4 characters are rejected from
+        // encrypting
         public static string testLessThan4CharInputWord()
         {
             encryption.encryptWord wordLength = new encryptWord();
             return wordLength.Encrypt("abc");
         }
 
+        // Test to ensure strings of length 0 are rejected from encrypting
         public static string testZeroCharInputWord()
         {
             encryption.encryptWord wordLength = new encryptWord();
             return wordLength.Encrypt("");
         }
 
+        // Test a low guess value. numberOfLowGuesses should increment and 
+        // average guess value should update to guess value
         public static string testLowGuessValue()
         {
             encryption.encryptWord lowGuess = new encryptWord();
@@ -43,6 +56,8 @@ namespace DriverFile
             return lowGuess.DisplayGuessStatistics();
         }
 
+        // Test a high guess value. numberOfHighGuesses should increment and 
+        // average guess value should update to guess value
         public static string testHighGuessValue()
         {
             encryption.encryptWord highGuess = new encryptWord();
@@ -50,6 +65,8 @@ namespace DriverFile
             return highGuess.DisplayGuessStatistics();
         }
 
+        // Test a correct SHIFT value. numberOfQueries should increment and 
+        // average guess value should update to guess value
         public static string testCorrectGuessValue()
         {
             encryption.encryptWord correctGuess = new encryptWord();
@@ -57,6 +74,21 @@ namespace DriverFile
             return correctGuess.DisplayGuessStatistics();
         }
 
+        // Test average guess value is working correctly. The two guesses 
+        // should be summed, divided by 2, then rounded down to the nearest
+        // whole number
+        public static string testAverageGuessValue()
+        {
+            encryption.encryptWord averageGuess = new encryptWord();
+            averageGuess.GuessShiftValue(2);
+            averageGuess.GuessShiftValue(5);
+            return averageGuess.DisplayGuessStatistics();
+        }
+
+        // Test resetting the EncryptWord object. Input a low guess, high guess
+        // and a correct guess. Then call ResetObject function. 
+        // DisplayGuessStatistics function then should have all statistics set
+        // to 0's.
         public static string testResetObject()
         {
             encryption.encryptWord resetTest = new encryptWord();
--- EncryptWord/Main.cs
+++ EncryptWord/Main.cs
@@ -3,63 +3,68 @@
 // DATE: 26Apr2018
 // VERSION: 1
 
+// This file will call Driver.cs. Driver.cs contains the test functions for the
+// EncryptWord class. Main.cs (this file) will interact with the user through
+// the console and will call the Driver.cs file to perfrom its tests.
+
 using System;
-using encryption;
+using DriverFile;
 
 class Program
 {
-    public static void WelcomeMessage()
-    {
-        Console.WriteLine("This program tests the functionality and state changes of the "
-                + "encryptWord class. \nEach function will start by printing a "
-                + "message to the screen describing what \ntest is being performed. "
-                + "Then the results of the test will be printed to the \nscreen as "
-                          + "well.\n");
-    }
-
+    // Entry point into the program
     public static void Main()
     {
-        WelcomeMessage();
+        // Introduce the program to the user
+        Console.WriteLine("This program tests the functionality and state "
+                          + "changes of the encryptWord class. \nEach "
+                          + "function will start by printing a message to the "
+                          + "screen describing what \ntest is being performed."
+                          + " Then the results of the test will be printed to "
+                          + "the \nscreen as well.\n");   
 
         // Test object initial state
-        Console.WriteLine("This will test the EncryptWord object initial state."
-                          + " If successful, EncryptWord's \nnumberOfQueries, "
-                          + "numberOfHighGuesses, numberOfLowGuesses, and "
-                          + "sumOfGuesses should \nbe 0.");
+        Console.WriteLine("Testing the EncryptWord object initial state.");
+        Console.WriteLine("All statistics should be set to 0's.");
         Console.WriteLine(DriverFile.Driver.testObjectInitialState());
 
         // Test end of alphabet encrypting properly
-        Console.WriteLine("\nThis will test the cipher value wrapping encryption back to the "
-        + "start of the \nalphabet. For example a 'z' will be encrypted as a "
-        + "letter at the beginning \nof the lowercase alphabet.\n");
+        Console.WriteLine("\nTesting x, y and z are encrypted properly as a, "
+                          + "b and c respectively.");
         Console.WriteLine("Encrypting the word: xxyyzz");
-        Console.WriteLine("Result: " + DriverFile.Driver.testEncryptEndOfAlphabet());
+        Console.WriteLine("Result: " 
+                          + DriverFile.Driver.testEncryptEndOfAlphabet());
 
         // Tests Encrypt function with a word less than 4 characters
-        Console.WriteLine("\nThis will test calling the Encrypt function with a "
-                          + "string less than 4 characters. \nThe word is "
-                          + "'abc'. An error message should be return rather "
-                          + "than an encrypted word.");
-        Console.WriteLine("Result: " + DriverFile.Driver.testLessThan4CharInputWord());
+        Console.WriteLine("\nTesting the Encrypt function rejecting "
+                          + "strings less than 4 characters.");
+        Console.WriteLine("Trying to encrypt \"abc\"");
+        Console.WriteLine("Result: " 
+                          + DriverFile.Driver.testLessThan4CharInputWord());
 
         // Tests Encrypt function with a zero character string
-        Console.WriteLine("\nThis will test calling the Encrypt function with a "
-                          + "zero character string. \nAn error message "
-                          + "should be return rather than an encrypted word.");
-        Console.WriteLine("Result: " + DriverFile.Driver.testZeroCharInputWord());
-
-
+        Console.WriteLine("\nTesting the Encrypt function rejecting "
+                         + "strings of length 0.");
+        Console.WriteLine("Trying to encrypt \"\"");
+        Console.WriteLine("Result: " 
+                          + DriverFile.Driver.testZeroCharInputWord());
+        
         // Tests guessing a shift value lower than the SHIFT value
-        Console.WriteLine("\nGuessing a shift of 0.");
+        Console.WriteLine("\nTesting a guess a shift of 0.");
         Console.WriteLine(DriverFile.Driver.testLowGuessValue());
 
         // Tests guessing a shift value higher than the SHIFT value
-        Console.WriteLine("\nGuessing a shift of 4.");
+        Console.WriteLine("\nTesting a guessy a shift of 4.");
         Console.WriteLine(DriverFile.Driver.testHighGuessValue());
 
         // Tests guessing a shift value equal to SHIFT value
-        Console.WriteLine("\nGuessing a shift of 3.");
+        Console.WriteLine("\nTesting a guess a shift of 3.");
         Console.WriteLine(DriverFile.Driver.testCorrectGuessValue());
+
+        // Tests average guess value of DisplayGuessStatistics function
+        Console.WriteLine("\nTesting average guess value of EncryptWord.");
+        Console.WriteLine("Guessing 2 and 5, average should be 3.");
+        Console.WriteLine(DriverFile.Driver.testAverageGuessValue());
 
         // Tests resetting the EncryptWord object
         Console.WriteLine("\nTesting ResetObject function.");
